6bbe56e134db16ef3cba868bbaec6b20304286ec
Abhishek Jha <akjktm75@gmail.com>
2021-03-06 09:13:49 +0545

    token generator

--- api/doctor/controllers/doctor.js
+++ api/doctor/controllers/doctor.js
@@ -1,8 +1,53 @@
-'use strict';
+const AccessToken = require("twilio").jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+("use strict");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  token(ctx) {
+    try {
+      const user = ctx.state.user;
+      if (user && user.userType === "Doctor") {
+        //send email to this patient.
+        const patientID = ctx.request.body.patientID;
+        // Used when generating any kind of Access Token
+        const twilioAccountSid = "SKc3525e2d755ab703560327f21d09286f";
+        const twilioApiKey = "";
+        const twilioApiSecret = "WkwzdeKhTNIDEmRE1DkOIfEgeCwTkXJs";
+
+        // Create an access token which we will sign and return to the client,
+        // containing the grant we just created
+        const token = new AccessToken(
+          twilioAccountSid,
+          twilioApiKey,
+          twilioApiSecret
+        );
+        token.identity = user.username;
+
+        // Create a Video grant which enables a client to use Video
+        // and limits access to the specified Room
+        const room = `${ctx.user.username}-${patientID}-Session`;
+        const videoGrant = new VideoGrant({
+          room: room,
+        });
+
+        // Add the grant to the token
+        token.addGrant(videoGrant);
+
+        // Serialize the token to a JWT string
+        const tokenString = token.toJwt();
+        console.log(tokenString);
+
+        return { token: tokenString };
+      } else {
+        ctx.send({ message: "Unauthorized" }, 403);
+      }
+    } catch (e) {
+      ctx.send({ message: "Internal Server Error" }, 500);
+    }
+  },
+};
--- api/patient/controllers/patient.js
+++ api/patient/controllers/patient.js
@@ -1,8 +1,45 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  token(ctx) {
+    try {
+      const room = ctx.request.body.room;
+      // Used when generating any kind of Access Token
+      const twilioAccountSid = "SKc3525e2d755ab703560327f21d09286f";
+      const twilioApiKey = "eELk8yELWkTLBBhvFpc37U9jRTK1hwxxNs89292Y";
+      const twilioApiSecret = "WkwzdeKhTNIDEmRE1DkOIfEgeCwTkXJs";
+
+      // Create an access token which we will sign and return to the client,
+      // containing the grant we just created
+      const token = new AccessToken(
+        twilioAccountSid,
+        twilioApiKey,
+        twilioApiSecret
+      );
+      token.identity = user.username;
+
+      // Create a Video grant which enables a client to use Video
+      // and limits access to the specified Room
+      const videoGrant = new VideoGrant({
+        room: room,
+      });
+
+      // Add the grant to the token
+      token.addGrant(videoGrant);
+
+      // Serialize the token to a JWT string
+      const tokenString = token.toJwt();
+      console.log(tokenString);
+
+      //Email the patient the room name.
+      return { token: tokenString, room: room };
+    } catch (e) {
+      ctx.send({ message: "Internal Server Error" }, 500);
+    }
+  },
+};
